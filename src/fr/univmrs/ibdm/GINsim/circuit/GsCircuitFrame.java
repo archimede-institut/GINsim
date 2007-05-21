6d589ef79c67f1d38aab8e652939d747bf22a8e0
Aurelien Naldi <naldi@tagc.univ-mrs.fr>
2007-05-21 12:15:43 +0200

    if only one sub-circuit is finctional, show which one it is (add [1] and the likes to the summary line)

--- src/fr/univmrs/ibdm/GINsim/circuit/GsCircuitFrame.java
+++ src/fr/univmrs/ibdm/GINsim/circuit/GsCircuitFrame.java
@@ -977,42 +977,61 @@ class GsCircuitDescrInTree {
         if (circuit.t_vertex == null) {
             return "no name";
         }
+        int nbChild = 1;
+        int index;
         if (summary) {
-            String s = circuit.t_vertex[0].toString();
-            for (int i = 1; i < circuit.t_vertex.length; i++) {
+        	Object o = null;
+            switch (key) {
+            case GsCircuitDescr.ALL:
+            	nbChild = circuit.t_sub.length;
+                break;
+            case GsCircuitDescr.FUNCTIONNAL:
+            	nbChild = circuit.v_functionnal.size();
+            	o = circuit.v_functionnal.get(0);
+                break;
+            case GsCircuitDescr.POSITIVE:
+            	nbChild = circuit.v_positive.size();
+            	o = circuit.v_positive.get(0);
+                break;
+            case GsCircuitDescr.NEGATIVE:
+            	nbChild = circuit.v_negative.size();
+            	o = circuit.v_negative.get(0);
+                break;
+            case GsCircuitDescr.DUAL:
+            	nbChild = circuit.v_dual.size();
+            	o = circuit.v_dual.get(0);
+                break;
+            }
+            if (nbChild == 1 && o != null) {
+            		index = ((GsCircuitDescrInTree)o).key;
+            } else {
+            	index = 0;
+            }
+        } else {
+        	index = key;
+        }
+        String s = "";
+        // if the circuit has several children, then hide details
+        if (summary && nbChild > 1) {
+            for (int i=0 ; i < circuit.t_vertex.length ; i++) {
                 s += " " + circuit.t_vertex[i];
             }
-            if (circuit.t_sub != null && circuit.t_sub.length > 1) {
-                switch (key) {
-                    case GsCircuitDescr.ALL:
-                        s += "  ("+circuit.t_sub.length+")";
-                        break;
-                    case GsCircuitDescr.FUNCTIONNAL:
-                        s += "  ("+circuit.v_functionnal.size()+"/"+circuit.t_sub.length+")";
-                        break;
-                    case GsCircuitDescr.POSITIVE:
-                        s += "  ("+circuit.v_positive.size()+"/"+circuit.t_sub.length+")";
-                        break;
-                    case GsCircuitDescr.NEGATIVE:
-                        s += "  ("+circuit.v_negative.size()+"/"+circuit.t_sub.length+")";
-                        break;
-                    case GsCircuitDescr.DUAL:
-                        s += "  ("+circuit.v_dual.size()+"/"+circuit.t_sub.length+")";
-                        break;
-                }
-            }
-            return s;
+        } else { // if one child only, show details here and hide the child
+	        int[] t_pos;
+	        t_pos = circuit.t_sub[index];
+	        for (int i=0 ; i < circuit.t_vertex.length ; i++) {
+	            s += " " + circuit.t_vertex[i];
+	            if (t_pos[i] != 0) {
+	                s += "["+t_pos[i]+"]";
+	            }
+	        }
         }
-        int[] t_pos = circuit.t_sub[key];
-        String s = circuit.t_vertex[0].toString();
-        if (t_pos[0] != 0) {
-            s += "["+t_pos[0]+"]";
-        }
-        for (int i = 1; i < circuit.t_vertex.length; i++) {
-            s += " " + circuit.t_vertex[i];
-            if (t_pos[i] != 0) {
-                s += "["+t_pos[i]+"]";
-            }
+        if (summary && circuit.t_sub != null && circuit.t_sub.length > 1) {
+        	if (circuit.t_sub.length == nbChild) {
+                s += "  ("+nbChild+")";
+        	} else {
+                s += "  ("+nbChild+"/"+circuit.t_sub.length+")";
+        	} 
         }
         return s;
     }
