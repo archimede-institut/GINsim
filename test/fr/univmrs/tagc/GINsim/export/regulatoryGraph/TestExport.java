ec0ea16ce1070a3c619c57e6ca454315a3a6cf52
Berenguier Duncan <elaum@free.fr>
2008-03-14 16:15:22 +0100

    Fixed TestExport.java First stable release for GNAMLExport.

--- test/fr/univmrs/tagc/GINsim/export/regulatoryGraph/TestExport.java
+++ test/fr/univmrs/tagc/GINsim/export/regulatoryGraph/TestExport.java
@@ -1,26 +1,29 @@
 package fr.univmrs.tagc.GINsim.export.regulatoryGraph;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import junit.framework.TestCase;
 import fr.univmrs.tagc.GINsim.export.GsExportConfig;
-import fr.univmrs.tagc.GINsim.graph.GsGinsimGraphDescriptor;
+import fr.univmrs.tagc.GINsim.graph.GsGinmlParser;
 import fr.univmrs.tagc.GINsim.graph.GsGraph;
-import fr.univmrs.tagc.GINsim.graph.GsGraphDescriptor;
+import fr.univmrs.tagc.common.TestTools;
 
 public class TestExport extends TestCase {
 	GsGraph graph;
-	static String root = "testCase/";
+	File tmpDir = TestTools.getTempDir();
 	
-	public void textExport() {
-        GsGraphDescriptor gd = new GsGinsimGraphDescriptor();
-        graph = gd.open(new File(root+"graph.ginml"));
+	public TestExport() throws FileNotFoundException {
+		File file = new File(TestTools.getTestDir(), "graph.ginml");
+		GsGinmlParser parser = new GsGinmlParser();
+		this.graph = parser.parse(new FileInputStream(file), null);
 	}
-
-	public void testGNAML() {
+	public void testGNAML() throws IOException {
 		GsGNAMLExport export =  new GsGNAMLExport();
 		GsExportConfig config = new GsExportConfig(graph, export, 0);
-		config.setFilename(root+"graph.gnaml");
+		config.setFilename(TestTools.getTestDir().getAbsolutePath()+File.separator+"tmp/graph.gnaml"); //FIXME: replace TestTools.getTtestDir() by tmpDir
 		export.doExport(config);
 	}
 }
