19723534ccc437aef6648039e4029101303d0950
Duncan Berenguier <berenguier@tagc.univ-mrs.fr>
2009-01-27 11:04:10 +0100

    add a interaction analysis plugin (functionality check only for now)

---
+++ src/fr/univmrs/tagc/GINsim/interactionAnalysis/SearchNonFunctionalInteractionsPlugin.java
@@ -0,0 +1,39 @@
+package fr.univmrs.tagc.GINsim.interactionAnalysis;
+
+import javax.swing.JFrame;
+
+import fr.univmrs.tagc.GINsim.graph.GsActionProvider;
+import fr.univmrs.tagc.GINsim.graph.GsGraph;
+import fr.univmrs.tagc.GINsim.gui.GsPluggableActionDescriptor;
+import fr.univmrs.tagc.GINsim.plugin.GsPlugin;
+import fr.univmrs.tagc.common.GsException;
+
+public class SearchNonFunctionalInteractionsPlugin implements GsActionProvider, GsPlugin {
+
+	private GsPluggableActionDescriptor[] t_action = null;
+
+	public void registerPlugin() {
+		GsGraph.registerActionProvider(this);
+	}
+
+	
+	public GsPluggableActionDescriptor[] getT_action(int actionType, GsGraph graph) {
+		if (actionType != ACTION_ACTION) {
+			return null;
+		}
+		if (t_action == null) {
+			t_action = new GsPluggableActionDescriptor[1];
+			t_action[0] = new GsPluggableActionDescriptor("STR_snfi", "STR_snfi_descr", null, this, ACTION_ACTION, 0);
+		}
+		return t_action;
+	}
+	
+	public void runAction(int actionType, int ref, GsGraph graph, JFrame frame) throws GsException {
+		if (actionType != ACTION_ACTION) {
+			return;
+		}
+		if (ref == 0) {
+           new SearchNonFunctionalInteractionsFrame(frame, graph);
+		}
+	}
+}
