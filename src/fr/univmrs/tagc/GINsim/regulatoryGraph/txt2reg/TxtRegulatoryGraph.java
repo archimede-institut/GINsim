6de5c7ad71b42ba04c479cf0480f5696effceeaf
Bahia Behba <b__bhb@hotmail.fr>
2011-09-29 16:40:20 +0100

    NEW: Initial version of the Truthtable-to-regulatory-graph implemented by Bahia

---
+++ src/fr/univmrs/tagc/GINsim/regulatoryGraph/txt2reg/TxtRegulatoryGraph.java
@@ -0,0 +1,19 @@
+package fr.univmrs.tagc.GINsim.regulatoryGraph.txt2reg;
+import java.awt.event.KeyEvent;
+
+
+/**
+ * The model
+ */
+
+public class TxtRegulatoryGraph {
+	 
+	/**
+	 * @param args
+	 */
+
+		// TODO Auto-generated method stub
+
+	}
+
+
