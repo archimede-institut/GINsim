22849b4a11300263e9127cd0c054ce6a7e72fc89
Lionel Spinelli <spinelli@tagc.univ-mrs.fr>
2011-11-10 17:11:27 +0100

    Refactoring - Day 4 : Atomic commit of service declaration fix on StableStatesService

--- src/org/ginsim/service/action/stablestates/StableStatesService.java
+++ src/org/ginsim/service/action/stablestates/StableStatesService.java
@@ -4,6 +4,8 @@ import java.util.Iterator;
 import java.util.List;
 
 import org.ginsim.graph.Graph;
+import org.ginsim.service.GsService;
+import org.mangosdk.spi.ProviderFor;
 
 import fr.univmrs.tagc.GINsim.data.GsDirectedEdge;
 import fr.univmrs.tagc.GINsim.regulatoryGraph.GsRegulatoryGraph;
@@ -19,11 +21,12 @@ import fr.univmrs.tagc.GINsim.regulatoryGraph.mutant.Perturbation;
  * To find a stable state, one can build a MDD for each gene, giving the context under which 
  * THIS gene is stable.Then the stable states can be found by combining these diagrams.
  * 
- * To improve performances, the individuals "stability" MDD are not built independantly 
+ * To improve performances, the individuals "stability" MDD are not built independently 
  * but immediately assembled.
  * The order in which they are considerd is also chosen to keep them small as long as possible.
  */
-public class StableStatesService {
+@ProviderFor( GsService.class)
+public class StableStatesService implements GsService{
 
 	private GsRegulatoryGraph regGraph;
 	List nodeOrder;
@@ -38,6 +41,10 @@ public class StableStatesService {
 	
 	int bestIndex, bestValue;
 	int nbgene, nbremain;
+	
+	public StableStatesService() {
+		// TODO Auto-generated constructor stub
+	}
 
 	public StableStatesService( Graph regGraph, List nodeOrder, Perturbation mutant) {
 		
