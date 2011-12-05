96947526c2c6bca946f5f89d85724771b2dea979
Duncan Berenguier <duncan.berenguier@gmail.com>
2011-12-05 13:00:53 +0100

    Refactoring - Day 21 : remove dynamicalHierarchicalGraph, dynamicalHierarchicalSimplifier and STG2HTG

--- src/org/ginsim/service/tool/dynamicalhierarchicalsimplifier/DynamicalHierarchicalSimplifierService.java
+++
@@ -1,9 +0,0 @@
-package org.ginsim.service.tool.dynamicalhierarchicalsimplifier;
-
-import org.ginsim.service.Service;
-import org.mangosdk.spi.ProviderFor;
-
-@ProviderFor( Service.class)
-public class DynamicalHierarchicalSimplifierService implements Service {
-
-}
