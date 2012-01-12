dd71b900b0ed848a372cb4c220cd31ebb5bc10c1
Duncan Berenguier <duncan.berenguier@gmail.com>
2012-01-12 08:36:04 +0100

    Unitary Test : Add tests on logical parameters in regulatory Graph

--- test/org/ginsim/core/graph/regulatorygraph/BasicRegulatoryGraphTest.java
+++ test/org/ginsim/core/graph/regulatorygraph/BasicRegulatoryGraphTest.java
@@ -15,6 +15,8 @@ import org.ginsim.core.graph.GraphManager;
 import org.ginsim.core.graph.common.EdgeAttributeReaderImpl;
 import org.ginsim.core.graph.common.Graph;
 import org.ginsim.core.graph.common.NodeAttributeReaderImpl;
+import org.ginsim.core.graph.regulatorygraph.logicalfunction.LogicalParameter;
+import org.ginsim.core.graph.regulatorygraph.omdd.OMDDNode;
 import org.ginsim.core.graph.view.NodeBorder;
 import org.ginsim.core.graph.view.NodeShape;
 
@@ -195,6 +197,64 @@ public class BasicRegulatoryGraphTest {
 		assertEquals( "Remove MultiEdge : the graph does not contains the right number of edges.", 0, multiedge_count);
 		
 		
+	}
+	
+	/**
+	 * Create two multivaluated nodes with a Multiedge and some logical parameters.
+	 * 
+	 */
+	@Test public void addLogicalParametersTest() {
+		
+		// Create a new RegulatoryGraph
+		RegulatoryGraph regGraph = GraphManager.getInstance().getNewGraph();
+		assertNotNull( "Create graph : the created graph is null: creation failed.", regGraph);
+		
+		// Add nodes G0 and G1
+		RegulatoryNode node_g0 = regGraph.addNode();
+		RegulatoryNode node_g1 = regGraph.addNode();
+		node_g1.setMaxValue((byte) 2, regGraph);
+
+		// Add MultiEdge
+		RegulatoryMultiEdge g0_g1 = regGraph.addEdge(node_g0, node_g1, RegulatoryEdgeSign.POSITIVE);
+		RegulatoryMultiEdge g0_g0 = regGraph.addEdge(node_g0, node_g0, RegulatoryEdgeSign.POSITIVE);
+		RegulatoryMultiEdge g1_g0 = regGraph.addEdge(node_g1, node_g0, RegulatoryEdgeSign.POSITIVE);
+		RegulatoryMultiEdge g1_g1 = regGraph.addEdge(node_g1, node_g1, RegulatoryEdgeSign.POSITIVE);
+		try {
+			regGraph.addNewEdge( "G1", "G0", (byte) 2, RegulatoryEdgeSign.POSITIVE);
+			regGraph.addNewEdge( "G1", "G1", (byte) 2, RegulatoryEdgeSign.POSITIVE);
+		} catch (GsException e) {
+			fail("Cannot add a multiedge");
+		}
+
+		//Testing the default parameters added to the G0
+		OMDDNode parameters = node_g0.getTreeParameters(regGraph);
+		assertNotNull( "Initial parameters of a node is null.", parameters);
+		assertEquals( "Initial parameter is not 0", parameters, OMDDNode.TERMINALS[0]);
+		
+		
+		LogicalParameter lp;
+		//Create logical parameters for G0
+		lp = new LogicalParameter(1); //G1:2 G0
+		lp.addEdge(g1_g0.getEdge(1));
+		lp.addEdge(g0_g0.getEdge(0));
+		node_g0.addLogicalParameter(lp, true);
+		lp = new LogicalParameter(1); //G1
+		lp.addEdge(g1_g0.getEdge(0));
+		node_g0.addLogicalParameter(lp, true);
+		assertTrue("The logical parameters of g0 doesn't match", node_g0.getTreeParameters(regGraph).toString().equals("((N[0]=0 && ((N[1]=0 && 0) ; (N[1]=1 && 1) ; (N[1]=2 && 0))) ; (N[0]=1 && ((N[1]=0 && 0) ; (N[1]=1 && 0) ; (N[1]=2 && 1))))"));
+
+		//Create logical parameters for G1
+		lp = new LogicalParameter(1); //G1:2 G0
+		lp.addEdge(g0_g1.getEdge(0));
+		lp.addEdge(g1_g1.getEdge(0));
+		node_g1.addLogicalParameter(lp, true);
+		lp = new LogicalParameter(1); //G1
+		lp.addEdge(g0_g1.getEdge(0));
+		node_g1.addLogicalParameter(lp, true);
+		assertTrue("The logical parameters of g1 doesn't match", node_g1.getTreeParameters(regGraph).toString().equals("((N[0]=0 && 0) ; (N[0]=1 && ((N[1]=0 && 1) ; (N[1]=1 && 1) ; (N[1]=2 && 0))))"));
+
+		
+		GraphManager.getInstance().close( regGraph);
 	}
 	
 
