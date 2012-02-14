236b143b0035aab329192ae7e22e784cc8e19f81
Aurelien Naldi <aurelien.naldi@gmail.com>
2012-02-14 16:55:05 +0100

    MOD: fun with generics in the ReducedGraphFactory. Now ant should be able to compile again

--- src/org/ginsim/core/graph/reducedgraph/ReducedGraphFactory.java
+++ src/org/ginsim/core/graph/reducedgraph/ReducedGraphFactory.java
@@ -1,5 +1,6 @@
 package org.ginsim.core.graph.reducedgraph;
 
+import org.ginsim.core.graph.common.Edge;
 import org.ginsim.core.graph.common.Graph;
 import org.ginsim.core.graph.common.GraphFactory;
 import org.mangosdk.spi.ProviderFor;
@@ -10,9 +11,9 @@ import org.mangosdk.spi.ProviderFor;
  * descriptor for regulatoryGraph.
  */
 @ProviderFor( GraphFactory.class)
-public class ReducedGraphFactory implements GraphFactory<ReducedGraph> {
+public class ReducedGraphFactory<G extends Graph<V,E>, V, E extends Edge<V>> implements GraphFactory<ReducedGraph<G,V,E>> {
 
-    private static ReducedGraphFactory instance = null;
+    private static ReducedGraphFactory<?,?,?> instance = null;
     
     public ReducedGraphFactory() {
     	
@@ -24,7 +25,7 @@ public class ReducedGraphFactory implements GraphFactory<ReducedGraph> {
     /**
      * @return an instance of this graphDescriptor.
      */
-    public static ReducedGraphFactory getInstance() {
+    public static ReducedGraphFactory<?,?,?> getInstance() {
         if (instance == null) {
             instance = new ReducedGraphFactory();
         }
@@ -37,13 +38,14 @@ public class ReducedGraphFactory implements GraphFactory<ReducedGraph> {
     }
     
     @Override
-	public Class<ReducedGraph> getGraphClass(){
-		return ReducedGraph.class;
+	public Class<ReducedGraph<G,V,E>> getGraphClass(){
+    	Class<? extends ReducedGraph> cl = ReducedGraph.class;
+		return (Class<ReducedGraph<G, V, E>>)cl;
 	}
     
     
     @Override
-    public Class getParser() {
+    public Class<ReducedGraphParser> getParser() {
     	return ReducedGraphParser.class;
     }
     
