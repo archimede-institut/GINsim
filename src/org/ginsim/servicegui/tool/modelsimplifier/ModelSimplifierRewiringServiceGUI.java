eab2746c0666557ae36ff79b0d81121053f75735
Duncan Berenguier <duncan.berenguier@gmail.com>
2012-04-02 15:25:13 +0200

    MOD : separate servicesGUI of ModelSimplifier and Rewiring, pass the second's status to UNDER_DEVELPPMENT

--- src/org/ginsim/servicegui/tool/modelsimplifier/ModelSimplifierRewiringServiceGUI.java
+++ src/org/ginsim/servicegui/tool/modelsimplifier/ModelSimplifierRewiringServiceGUI.java
@@ -14,6 +14,7 @@ import org.ginsim.gui.service.ServiceGUI;
 import org.ginsim.gui.service.common.GUIFor;
 import org.ginsim.gui.service.common.ServiceStatus;
 import org.ginsim.gui.service.common.ToolAction;
+import org.ginsim.gui.utils.dialog.stackdialog.HandledStackDialog;
 import org.ginsim.service.tool.modelsimplifier.ModelSimplifierService;
 import org.mangosdk.spi.ProviderFor;
 
@@ -22,14 +23,14 @@ import org.mangosdk.spi.ProviderFor;
  */
 @ProviderFor(ServiceGUI.class)
 @GUIFor(ModelSimplifierService.class)
-@ServiceStatus( ServiceStatus.RELEASED)
-public class ModelSimplifierServiceGUI extends AbstractServiceGUI {
+@ServiceStatus( ServiceStatus.UNDER_DEVELOPMENT)
+public class ModelSimplifierRewiringServiceGUI extends AbstractServiceGUI {
 
 	@Override
 	public List<Action> getAvailableActions(Graph<?, ?> graph) {
 		if (graph instanceof RegulatoryGraph) {
 			List<Action> actions = new ArrayList<Action>();
-			actions.add(new ModelSimplifierAction((RegulatoryGraph)graph, this));
+			actions.add(new ModelRewiringAction((RegulatoryGraph)graph, this));
 			return actions;
 		}
 		return null;
@@ -37,16 +38,15 @@ public class ModelSimplifierServiceGUI extends AbstractServiceGUI {
 
 	@Override
 	public int getInitialWeight() {
-		return W_TOOLS_MAIN + 50;
+		return W_TOOLS_MAIN + 51;
 	}
 }
 
-class ModelSimplifierAction extends ToolAction {
-
-	private static final long serialVersionUID = 2341667285128867588L;
+class ModelRewiringAction extends ToolAction {
+	private static final long serialVersionUID = 2437799457065501757L;
 	private final RegulatoryGraph graph;
-	public ModelSimplifierAction(RegulatoryGraph graph, ServiceGUI serviceGUI) {
-		super("STR_reduce", "STR_reduce_descr", serviceGUI);
+	public ModelRewiringAction(RegulatoryGraph graph, ServiceGUI serviceGUI) {
+		super("STR_rewire", "STR_rewire_descr", serviceGUI);
 		this.graph = graph;
 	}
 
@@ -58,7 +58,6 @@ class ModelSimplifierAction extends ToolAction {
 		}
 
 		// TODO: reset edit mode
-		// mframe.getActions().setCurrentMode(GsActions.MODE_DEFAULT, 0, false);
-		new ModelSimplifierConfigDialog(graph);
+		new HandledStackDialog(new RewiringGUI( graph));
 	}
 }
