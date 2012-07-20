5658d26b67ada2e179b041b9126af52983bdd63b
Aurélien Naldi <aurelien.naldi@gmail.com>
2012-07-20 14:09:17 +0200

    ADD service method to use the JSBML-based import/export

--- src/org/ginsim/service/export/sbml/SBMLQualExportService.java
+++ src/org/ginsim/service/export/sbml/SBMLQualExportService.java
@@ -1,7 +1,15 @@
 package org.ginsim.service.export.sbml;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 
+import javax.xml.stream.XMLStreamException;
+
+import org.colomoto.logicalmodel.LogicalModel;
+import org.colomoto.logicalmodel.io.sbml.SBMLFormat;
+import org.colomoto.logicalmodel.io.sbml.SBMLqualExport;
 import org.ginsim.core.graph.regulatorygraph.RegulatoryGraph;
 import org.ginsim.core.service.Alias;
 import org.ginsim.core.service.Service;
@@ -35,5 +43,22 @@ public class SBMLQualExportService implements Service{
 	public void export( RegulatoryGraph graph, String filename) throws IOException {
 		SBMLQualConfig cfg = new SBMLQualConfig(graph);
 		export(graph, cfg, filename);
+	}
+
+	/**
+	 * Export using the JSBML encoder for logical models.
+	 * 
+	 * @param graph
+	 * @param filename
+	 * @throws IOException
+	 */
+	public void exportJSBML( RegulatoryGraph graph, String filename) throws IOException {
+		LogicalModel model = graph.getModel();
+		OutputStream out = new FileOutputStream(new File(filename));
+		try {
+			new SBMLqualExport(model).export(out);
+		} catch (XMLStreamException e) {
+			throw new IOException(e);
+		}
 	}
 }
