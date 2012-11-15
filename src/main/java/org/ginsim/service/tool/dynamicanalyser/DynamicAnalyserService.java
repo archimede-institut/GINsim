8cdff82b509587ead8c7156e54b94c6982a73be5
Aurélien Naldi <aurelien.naldi@gmail.com>
2012-11-15 10:23:34 +0100

    MOD: remove empty services, turn GUIs into StandaloneGUIs

--- src/main/java/org/ginsim/service/tool/dynamicanalyser/DynamicAnalyserService.java
+++
@@ -1,9 +0,0 @@
-package org.ginsim.service.tool.dynamicanalyser;
-
-import org.ginsim.core.service.Service;
-import org.mangosdk.spi.ProviderFor;
-
-@ProviderFor( Service.class)
-public class DynamicAnalyserService implements Service {
-
-}
--- src/main/java/org/ginsim/service/tool/stateinregulatorygraph/StateInRegulatoryGraphService.java
+++
@@ -1,9 +0,0 @@
-package org.ginsim.service.tool.stateinregulatorygraph;
-
-import org.ginsim.core.service.Service;
-import org.mangosdk.spi.ProviderFor;
-
-@ProviderFor( Service.class)
-public class StateInRegulatoryGraphService implements Service {
-
-}
