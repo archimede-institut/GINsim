49d98e361fa2fe499d10be9bc56addbfbfbf39fe
Pedro Tiago Monteiro <ptgm@igc.gulbenkian.pt>
2013-06-15 15:58:38 +0100

    NEW: export capability for Avatar/Firefront algorithms. Refs #235

---
+++ src/main/java/org/ginsim/service/export/avatar/AvatarExportService.java
@@ -0,0 +1,35 @@
+package org.ginsim.service.export.avatar;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.ginsim.common.application.GsException;
+import org.ginsim.core.service.Alias;
+import org.ginsim.core.service.Service;
+import org.mangosdk.spi.ProviderFor;
+
+/**
+ * GINsim export service capable of encoding the working model into an AVATAR
+ * specification.
+ * 
+ * @author Pedro T. Monteiro
+ */
+@ProviderFor(Service.class)
+@Alias("Avatar")
+public class AvatarExportService implements Service {
+
+	public void run(AvatarConfig config, String filename) throws IOException, GsException {
+		File f = new File(filename);
+		export(config, f);
+	}
+	public void export(AvatarConfig config, File file) throws IOException, GsException {
+
+		FileWriter writer = new FileWriter(file);
+
+		AvatarEncoder encoder = new AvatarEncoder();
+		encoder.write(config, writer);
+
+		writer.close();
+	}
+}
