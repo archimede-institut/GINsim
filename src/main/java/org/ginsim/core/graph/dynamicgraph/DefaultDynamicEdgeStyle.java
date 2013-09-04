7484e0b938fddf693629fc6ccca4ef4ac0f2a4ca
Aurélien Naldi <aurelien.naldi@gmail.com>
2013-09-04 18:02:56 +0200

    MOD: add properties for edge colors in STG

--- src/main/java/org/ginsim/core/graph/dynamicgraph/DefaultDynamicEdgeStyle.java
+++ src/main/java/org/ginsim/core/graph/dynamicgraph/DefaultDynamicEdgeStyle.java
@@ -4,9 +4,22 @@ import java.awt.Color;
 
 import org.ginsim.core.graph.view.EdgePattern;
 import org.ginsim.core.graph.view.style.EdgeStyleImpl;
+import org.ginsim.core.graph.view.style.StyleProperty;
 
 public class DefaultDynamicEdgeStyle extends EdgeStyleImpl<DynamicNode, DynamicEdge> {
 
+	private static final StyleProperty PROP_INCREASE = StyleProperty.createColorProperty("inc");
+	private static final StyleProperty PROP_DECREASE = StyleProperty.createColorProperty("dec");
+	
+	private static final StyleProperty[] EXTRA_PROPERTIES = {
+		PROP_INCREASE,
+		PROP_DECREASE,
+	};
+	private static final StyleProperty[] PROPERTIES = StyleProperty.merge(EdgeStyleImpl.DEFAULT_PROPERTIES, EXTRA_PROPERTIES);
+
+	private Color c_inc = new Color(0,200,0);
+	private Color c_dec = new Color(200,0,0);
+	
 	@Override
 	public EdgePattern getPattern(DynamicEdge edge) {
 		switch (edge.changeType) {
@@ -23,10 +36,10 @@ public class DefaultDynamicEdgeStyle extends EdgeStyleImpl<DynamicNode, DynamicE
 		switch (edge.changeType) {
 		case INCREASE:
 		case MULTIPLE_INCREASE:
-			return Color.GREEN;
+			return c_inc;
 		case DECREASE:
 		case MULTIPLE_DECREASE:
-			return Color.RED;
+			return c_dec;
 		}
 		return super.getColor(edge);
 	}
@@ -36,5 +49,33 @@ public class DefaultDynamicEdgeStyle extends EdgeStyleImpl<DynamicNode, DynamicE
 		return true;
 	}
 
-	
+	@Override
+	public StyleProperty[] getProperties() {
+		return PROPERTIES;
+	}
+
+	@Override
+	protected Object getCustomProperty(StyleProperty prop) {
+		if (prop == PROP_INCREASE) {
+			return c_inc;
+		}
+		if (prop == PROP_DECREASE) {
+			return c_dec;
+		}
+		return null;
+	}
+
+	@Override
+	protected void setCustomProperty(StyleProperty prop, Object value) {
+		if (value == null) {
+			return;
+		}
+		
+		if (prop == PROP_INCREASE) {
+			c_inc = (Color)value;
+		}
+		if (prop == PROP_DECREASE) {
+			c_dec = (Color)value;
+		}
+	}
 }
