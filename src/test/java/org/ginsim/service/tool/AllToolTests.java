b1020f722fca94dc0b916606b9385c06e61871f6
Aurélien Naldi <aurelien.naldi@gmail.com>
2013-12-09 17:32:39 +0100

    CLEANUP: remove tests for the old simplifier tool

--- src/test/java/org/ginsim/service/tool/AllToolTests.java
+++ src/test/java/org/ginsim/service/tool/AllToolTests.java
@@ -4,7 +4,6 @@ import org.ginsim.service.tool.composition.TestComposition;
 import org.ginsim.service.tool.connectivity.TestConnectivity;
 import org.ginsim.service.tool.graphcomparator.TestGraphComparator;
 import org.ginsim.service.tool.localgraph.TestLocalGraph;
-import org.ginsim.service.tool.modelsimplifier.TestModifier;
 import org.ginsim.service.tool.sccgraph.TestSCCGraph;
 import org.ginsim.service.tool.stablestates.TestStableStates;
 import org.junit.runner.RunWith;
@@ -13,7 +12,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ TestConnectivity.class, TestGraphComparator.class,
-		TestModifier.class, TestSCCGraph.class, TestLocalGraph.class,
+		TestSCCGraph.class, TestLocalGraph.class,
 		TestStableStates.class, TestComposition.class })
 public class AllToolTests {
 
