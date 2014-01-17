262260cd693c23644ba32d821fc4a9e8d45d4f79
Aurelien Naldi <aurelien.naldi@gmail.com>
2014-01-17 20:58:53 +0100

    MOD: add signals for the new ListenableList

--- src/main/java/org/ginsim/core/utils/data/ListenableNamedList.java
+++ src/main/java/org/ginsim/core/utils/data/ListenableNamedList.java
@@ -1,11 +1,51 @@
 package org.ginsim.core.utils.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Aurelien Naldi
  */
 public class ListenableNamedList<T extends NamedObject> extends NamedList<T> {
 
+    private final List<GenericListListener> listeners = new ArrayList<GenericListListener>();
+
+
     public void addListListener(GenericListListener l) {
-        // FIXME: implement listenable lists!!
+        listeners.add(l);
+    }
+    public void removeListListener(GenericListListener l) {
+        listeners.remove(l);
+    }
+
+    private void fireAdded(T item, int idx) {
+        for (GenericListListener l: listeners) {
+            l.itemAdded(item, idx);
+        }
+    }
+
+    public T remove(int idx) {
+        T item = super.remove(idx);
+        if (item != null) {
+            for (GenericListListener l: listeners) {
+                l.itemRemoved(item, idx);
+            }
+        }
+        return item;
+    }
+
+    @Override
+    public boolean add(T t) {
+        boolean b = super.add(t);
+        if (b) {
+            fireAdded(t, size()-1);
+        }
+        return b;
+    }
+
+    @Override
+    public void add(int idx, T t) {
+        super.add(idx, t);
+        fireAdded(t, idx);
     }
 }
