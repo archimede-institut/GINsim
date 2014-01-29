843f5d58087e32f8b2d88fb2c76b6732a954fe18
Nuno Mendes <ndm@igc.gulbenkian.pt>
2014-01-29 17:41:47 +0000

    Added toString() method to CADPReadMeWriter. simplified IntegrationGrammer methods

--- src/main/java/org/ginsim/service/export/cadp/CADPReadMeWriter.java
+++ src/main/java/org/ginsim/service/export/cadp/CADPReadMeWriter.java
@@ -13,4 +13,32 @@ public class CADPReadMeWriter extends CADPWriter {
 		super(config);
 	}
 
+	public String toString() {
+
+		String out = "";
+
+		out += "*** README ***\n\n";
+		out += "\nThis file contains the intructions to invoke CADP to obtain the minimised composed LTS for the current composition and subsequently test the reachability of each stable state of the composed model.\n";
+		
+		out += "\n\nIn this bundle you will find the following files:\n";
+		out += "\t";
+		out += "\t";
+		// ...
+		
+		out += "\n\n";
+		
+		out += "To calculate the minimised composed LTS it suffices to invoke:\n";
+		out += "\t\t svl \n\n";
+		
+		// ...
+		
+
+		
+		// out to test reachability
+		// list stable states
+
+		return out;
+
+	}
+
 }
