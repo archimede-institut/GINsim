3a1d82d7c33ff80398cb7edd17a05529d8d26094
Aurélien Naldi <aurelien.naldi@gmail.com>
2014-02-04 10:36:22 +0100

    MOD: provide a Task for Circuit Analysis

---
+++ src/main/java/org/ginsim/service/tool/circuit/CircuitAnalyser.java
@@ -0,0 +1,53 @@
+package org.ginsim.service.tool.circuit;
+
+import org.colomoto.common.task.AbstractTask;
+import org.ginsim.core.graph.regulatorygraph.RegulatoryGraph;
+import org.ginsim.core.graph.regulatorygraph.RegulatoryNode;
+import org.ginsim.core.graph.regulatorygraph.perturbation.Perturbation;
+
+import java.util.List;
+
+/**
+ * A Task for circuit analysis: returns a list of circuits with functionality contexts.
+ *
+ * @author Aurelien Naldi
+ */
+public class CircuitAnalyser extends AbstractTask<List<CircuitDescrInTree>> {
+
+    private final List<CircuitDescrInTree> circuits;
+    private final CircuitSearcher searcher;
+
+    private final CircuitAlgo algo;
+    private final List<RegulatoryNode> nodeOrder;
+
+
+    public CircuitAnalyser(RegulatoryGraph graph, Perturbation perturbation, boolean do_cleanup) {
+        this(graph, perturbation, do_cleanup, null);
+    }
+
+    public CircuitAnalyser(RegulatoryGraph graph, Perturbation perturbation, boolean do_cleanup, List<CircuitDescrInTree> circuits) {
+        this.circuits = circuits;
+        if (circuits == null) {
+            searcher = new CircuitSearcher(graph);
+        } else {
+            searcher = null;
+        }
+        this.algo = new CircuitAlgo(graph, perturbation, do_cleanup);
+        this.nodeOrder = graph.getNodeOrder();
+    }
+
+    @Override
+    protected List<CircuitDescrInTree> doGetResult() {
+        List<CircuitDescrInTree> circuits = this.circuits;
+        if (circuits == null) {
+            circuits = searcher.call();
+        }
+
+        for (CircuitDescrInTree cdt: circuits) {
+            CircuitDescr cd = cdt.getCircuit();
+            cd.check(algo, nodeOrder);
+        }
+
+        return circuits;
+    }
+}
--- src/main/java/org/ginsim/service/tool/circuit/CircuitService.java
+++ src/main/java/org/ginsim/service/tool/circuit/CircuitService.java
@@ -1,30 +1,30 @@
 package org.ginsim.service.tool.circuit;
 
 import org.ginsim.core.graph.regulatorygraph.RegulatoryGraph;
+import org.ginsim.core.graph.regulatorygraph.perturbation.Perturbation;
 import org.ginsim.core.service.*;
 import org.mangosdk.spi.ProviderFor;
 
-import java.util.List;
 
 @ProviderFor( Service.class)
 @Alias("circuitAnalysis")
 @ServiceStatus(EStatus.RELEASED)
 public class CircuitService implements Service {
 
-    public CircuitAlgo getCircuitAnalyser(RegulatoryGraph graph) {
+    public CircuitAnalyser getCircuitAnalyser(RegulatoryGraph graph) {
         return getCircuitAnalyser(graph, false);
     }
 
-    public CircuitAlgo getCircuitAnalyser(RegulatoryGraph graph, boolean doCleanup) {
-        return new CircuitAlgo(graph, doCleanup);
+    public CircuitAnalyser getCircuitAnalyser(RegulatoryGraph graph, boolean doCleanup) {
+        return getCircuitAnalyser(graph, null, doCleanup);
+    }
+
+    public CircuitAnalyser getCircuitAnalyser(RegulatoryGraph graph, Perturbation perturbation, boolean doCleanup) {
+        return new CircuitAnalyser(graph, perturbation, doCleanup);
     }
 
     public CircuitSearcher getCircuitSearcher(RegulatoryGraph graph) {
         return new CircuitSearcher(graph);
-    }
-
-    public List<CircuitDescrInTree> getCircuits(RegulatoryGraph graph) {
-        return getCircuitSearcher(graph).call();
     }
 
 }
