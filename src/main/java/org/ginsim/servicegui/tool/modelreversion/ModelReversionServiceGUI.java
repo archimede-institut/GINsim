72e3f513d19f69df27a7ea3937779bbcbff6a791
Pedro T. Monteiro <pedro@pedromonteiro.org>
2015-07-13 16:28:10 +0100

    Model reverter now supports multivalued models (calls Booleanizer and issues a warning). Fixes #315

--- src/main/java/org/ginsim/servicegui/tool/modelreversion/ModelReversionServiceGUI.java
+++ src/main/java/org/ginsim/servicegui/tool/modelreversion/ModelReversionServiceGUI.java
@@ -6,7 +6,10 @@ import java.util.List;
 
 import javax.swing.Action;
 
+import org.colomoto.logicalmodel.LogicalModel;
+import org.colomoto.logicalmodel.tool.booleanize.Booleanizer;
 import org.colomoto.logicalmodel.tool.reverse.ModelReverser;
+import org.ginsim.commongui.dialog.GUIMessageUtils;
 import org.ginsim.core.graph.Graph;
 import org.ginsim.core.graph.regulatorygraph.LogicalModel2RegulatoryGraph;
 import org.ginsim.core.graph.regulatorygraph.RegulatoryGraph;
@@ -61,10 +64,17 @@ class ReversionAction extends ToolAction {
 	public void actionPerformed(ActionEvent arg0) {
 		ModelReversionService service = ServiceManager.getManager().getService(
 				ModelReversionService.class);
-		ModelReverser modelReverser = service.getModelReverser(graph);
+		LogicalModel origModel = this.graph.getModel();
+		ModelReverser modelReverser = service.getModelReverser(origModel
+				.isBoolean() ? origModel : Booleanizer.booleanize(origModel));
 		modelReverser.reverse();
 		RegulatoryGraph gReversed = LogicalModel2RegulatoryGraph
 				.importModel(modelReverser.getModel());
+
 		GUIManager.getInstance().whatToDoWithGraph(gReversed, true);
+		if (!origModel.isBoolean()) {
+			GUIMessageUtils.openErrorDialog("STR_revert_multivalue");
+		}
+
 	}
 }
