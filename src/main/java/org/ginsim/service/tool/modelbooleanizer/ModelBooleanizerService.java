aee427ab93105a22e068f3d3310ec9fb8000de7d
Pedro T. Monteiro <pedro@pedromonteiro.org>
2016-09-27 00:10:09 +0100

    Bug fix: nodes with _b in the name clashed with the automatically generated nodenames_bx yield by the procedure

--- src/main/java/org/ginsim/service/tool/modelbooleanizer/ModelBooleanizerService.java
+++ src/main/java/org/ginsim/service/tool/modelbooleanizer/ModelBooleanizerService.java
@@ -1,8 +1,10 @@
 package org.ginsim.service.tool.modelbooleanizer;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.colomoto.logicalmodel.LogicalModel;
 import org.colomoto.logicalmodel.tool.booleanize.Booleanizer;
-import org.ginsim.core.graph.regulatorygraph.LogicalModel2RegulatoryGraph;
 import org.ginsim.core.graph.regulatorygraph.RegulatoryGraph;
 import org.ginsim.core.graph.regulatorygraph.RegulatoryNode;
 import org.ginsim.core.graph.view.NodeAttributesReader;
@@ -34,13 +36,15 @@ public class ModelBooleanizerService implements Service {
 		NodeAttributesReader sNReader = src.getNodeAttributeReader();
 		NodeAttributesReader dNReader = dest.getNodeAttributeReader();
 
+		Pattern p = Pattern.compile(".*_b[1-9]$");
 		for (RegulatoryNode destNode : dest.getNodes()) {
 			dNReader.setNode(destNode);
 			RegulatoryNode srcNode = this.getEquivalentNode(src, destNode);
 			sNReader.setNode(srcNode);
 			dNReader.copyFrom(sNReader);
 			// If it's a multivalued node, then performs a displacement
-			if (destNode.getId().contains("_b")) {
+			Matcher m = p.matcher(destNode.getId());
+			if (m.find()) {
 				int i = Integer.parseInt(destNode.getId().substring(
 						destNode.getId().length() - 1)) - 1;
 				dNReader.move(this.DX * i, this.DY * i);
