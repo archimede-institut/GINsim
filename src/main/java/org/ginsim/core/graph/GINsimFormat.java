c8e7b5c39f24f86434e5e33cff3da8111461d168
Aurelien Naldi <aurelien.naldi@gmail.com>
2018-08-17 16:04:01 +0200

    Use the new Loader/Exporter API from bioLQM in the zginml bridge

--- src/main/java/org/ginsim/core/graph/GINsimFormat.java
+++ src/main/java/org/ginsim/core/graph/GINsimFormat.java
@@ -1,13 +1,12 @@
 package org.ginsim.core.graph;
 
-import java.io.IOException;
-
 import org.colomoto.biolqm.LogicalModel;
 import org.colomoto.biolqm.io.AbstractFormat;
-import org.colomoto.biolqm.io.StreamProvider;
-import org.ginsim.common.application.GsException;
+import org.colomoto.biolqm.io.BaseLoader;
+import org.colomoto.biolqm.io.ModelLoader;
 import org.ginsim.core.graph.regulatorygraph.RegulatoryGraph;
-import org.kohsuke.MetaInfServices;
+
+import java.io.IOException;
 
 
 // TODO: enable autodiscovery for this format when service files are properly merged
@@ -19,17 +18,20 @@ public class GINsimFormat extends AbstractFormat {
 	}
 
 	@Override
-	public LogicalModel load(StreamProvider ip) throws IOException {
-		
-		try {
-			Graph g = GSGraphManager.getInstance().open(ip.getFile());
-			if (g instanceof RegulatoryGraph) {
-				return ((RegulatoryGraph)g).getModel();
-			}
-			throw new IOException("The graph is not a logical model");
-		} catch (GsException e) {
-			throw new IOException(e);
-		}
+	public ModelLoader getLoader() {
+		return new GINsimLoader();
 	}
+}
 
+
+class GINsimLoader extends BaseLoader {
+
+	@Override
+	protected LogicalModel performTask() throws Exception {
+		Graph g = GSGraphManager.getInstance().open(streams.getFile());
+		if (g instanceof RegulatoryGraph) {
+			return ((RegulatoryGraph)g).getModel();
+		}
+		throw new IOException("The graph is not a logical model");
+	}
 }
