c82ace1634ef49043ee343a609c592a4783e01d1
Dominique Benielli <dominique.benielli@univ-amu.fr>
2024-10-25 11:43:38 +0200

    correct wrong change in name function

--- src/main/java/org/ginsim/gui/utils/widgets/treetable/TreeTableModelAdapter.java
+++ src/main/java/org/ginsim/gui/utils/widgets/treetable/TreeTableModelAdapter.java
@@ -89,7 +89,7 @@ public class TreeTableModelAdapter extends AbstractTableModel {
     	return treeTableModel.getColumnName(column);
     }
 
-    public Class getColgetEdgeViewInfoumnClass(int column) {
+    public Class getColumnClass(int column) {
     	return treeTableModel.getColumnClass(column);
     }
 
